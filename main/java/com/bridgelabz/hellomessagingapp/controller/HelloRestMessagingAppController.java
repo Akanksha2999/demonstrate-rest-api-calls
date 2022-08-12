a23d222e2c0c2f495448930b1240c03a01ce7d99
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-08-12 12:14:02 +0530

    ADDED: Name passed as query and displayed messaged

--- main/java/com/bridgelabz/hellomessagingapp/controller/HelloRestMessagingAppController.java
+++ main/java/com/bridgelabz/hellomessagingapp/controller/HelloRestMessagingAppController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +14,8 @@ public class HelloRestMessagingAppController {
         return "Hello From Bridgelabz!";
     }
 
+    @GetMapping("/query")
+    public String queryParam(@RequestParam String name){
+        return "Hello "+name+" From Bridgelabz";
+    }
 }
